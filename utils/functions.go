8b679033ee238f396e2c5190a4c8d4cb7fb974a0
wangxiangbo <wangxiangbo@cmsr.chinamobile.com>
2022-01-25 13:58:44 +0800

    提交协程运行safeGo方法

--- utils/functions.go
+++ utils/functions.go
@@ -1,8 +1,21 @@
 package utils
 
 import (
+	"go-timer-push/logger"
 	"time"
 )
+
+//对于协程内部运行的函数，如果发生panic会导致整个程序崩溃，故需要手动recover
+func SafeGo(do func()) {
+	go func() {
+		defer func() {
+			if err := recover(); err != nil {
+				logger.Logger.Error(err)
+			}
+		}()
+		do()
+	}()
+}
 
 //计算当前日期是第几周
 func WeekCountNow() int {
